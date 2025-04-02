98478206e864240a0d4c45b55d174945126a777c
madearga <madearga.work@gmail.com>
2025-04-02 21:55:52 +0700

    tes

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default async function RootLayout({
             __html: THEME_COLOR_SCRIPT,
           }}
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className="antialiased">
         <Providers>
